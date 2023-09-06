aef0d11696b1f399e0d594702d6a35846a1580f6
Anish Kumar <115490507+repo-by-anish@users.noreply.github.com>
2023-09-07 03:37:33 +0530

    Update messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -8,6 +8,8 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL,
         pass: process.env.MAIL_PASS,
     },
+    secureProtocol: 'TLSv1_2_method',
+    debug: true,
 });
 
 
