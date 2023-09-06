611be3247bb67cafe9edbb22a04b62dc0a5bfdb5
Anish Kumar <115490507+repo-by-anish@users.noreply.github.com>
2023-09-07 03:54:53 +0530

    Update messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,13 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: true,
+    secureConnection: false,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.MAIL_PASS,
     },
-    secureProtocol: 'TLSv1_2_method',
-    debug: true,
+        tls: {
+        ciphers:'SSLv3'
+    }
 });
 
 
